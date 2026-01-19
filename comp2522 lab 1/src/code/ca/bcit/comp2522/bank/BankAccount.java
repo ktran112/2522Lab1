f414f646a67d2de7e7578d105b28b6da67283e9d
umanga <umangabajgai@gmail.com>
2026-01-19 08:47:28 -0800

    committing before pulling

--- comp2522 lab 1/src/code/ca/bcit/comp2522/bank/BankAccount.java
+++ comp2522 lab 1/src/code/ca/bcit/comp2522/bank/BankAccount.java
@@ -1,4 +1,41 @@
 package ca.bcit.comp2522.bank;
 
 public class BankAccount {
+    double balanceUsd;
+    String accountNumber;
+    int pin;
+    BankClient client;
+    Date accountOpened;
+    Date accountClosed;
+    //has to have a clinet, a 6/7 character account number, has
+//    has a date for account opened, and has a date for account closed (can be null if not closed.)
+
+    /**
+     * Withdraw function using only amountUsd.
+     * @param amountUsd
+     */
+    private void withdraw(final double amountUsd) {
+
+    }
+
+    /**
+     * Withdraw function using amountUsd and pinToMatch.
+     * @param amountUsd
+     * @param pinToMatch
+     */
+    private void withdraw(final double amountUsd, final int pinToMatch) {
+
+    }
+
+    /**
+     * Returns string representing the bank account details.
+     */
+    private void getDetails() {
+        String details;
+        details = client.fullName
+        // amount usd. account number. opened date, closed date.
+        //need a getDetails method that retunrs a string excatly in the format:
+        // "Albert Einstein had $900 USD in account #abc123 which he
+        //opened on Monday January 1, 1900 and closed Saturday October 14, 1950.
+    }
 }
